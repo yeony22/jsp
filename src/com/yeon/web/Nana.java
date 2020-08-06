4854148ad230498bf5bdbbe17430c8f065a2c128
YeonGyeongLee <37134357+yeony22@users.noreply.github.com>
2020-08-06 17:48:48 +0900

    0806

---
+++ src/com/yeon/web/Nana.java
@@ -0,0 +1,38 @@
+package com.yeon.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hi")
+public class Nana extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+
+		PrintWriter out = response.getWriter();
+
+		String cnt_ = request.getParameter("cnt");
+		  
+		int cnt = 100; 
+		
+		// cnt_.equals("")
+		if(cnt_ != null) { 
+			cnt = Integer.parseInt(cnt_); 
+		}
+		 
+		//int cnt = Integer.parseInt(request.getParameter("cnt"));
+
+		for (int i = 0; i < cnt; i++) {
+			out.println((i + 1) + ": ¾È³ç Servlet!<br>");
+		}
+	}
+}
