2360c821672573c44ac43c9d4bc5d5cf362f79de
YeonGyeongLee <37134357+yeony22@users.noreply.github.com>
2020-08-10 19:00:30 +0900

    0810

--- src/com/yeon/web/Calc2.java
+++ src/com/yeon/web/Calc2.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/calc2")
 public class Calc2 extends HttpServlet {
@@ -16,6 +17,7 @@ public class Calc2 extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		ServletContext application = request.getServletContext();
+		HttpSession session = request.getSession();
 		
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF=8");
@@ -28,9 +30,11 @@ public class Calc2 extends HttpServlet {
 		
 		// 계산
 		if(op.equals("=")) {
-			int x = (Integer)application.getAttribute("value");
-			int y = v;
-			String operator = (String)application.getAttribute("op");
+			// int x = (Integer)application.getAttribute("value");
+			int x = (Integer)session.getAttribute("value");
+			int y = v;		
+			// String operator = (String)application.getAttribute("op");
+			String operator = (String)session.getAttribute("op");
 			int result = 0;
 			
 			if(operator.equals("+"))
@@ -43,8 +47,10 @@ public class Calc2 extends HttpServlet {
 		
 		// 값을 저장
 		else {
-			application.setAttribute("value", v);
-			application.setAttribute("op", op);
+			// application.setAttribute("value", v);
+			// application.setAttribute("op", op);
+			session.setAttribute("value", v);
+			session.setAttribute("op", op);
 		}		
 	}
 }
